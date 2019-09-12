75c13a72408e8d3d8cb69dcd2808f96b3b242cec
arseniyhost <pilipenko_arseniy@mail.ru>
2019-09-12 09:33:45 +0300

    xika

---
+++ Xika website/js/script.js
@@ -0,0 +1,25 @@
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+    document.getElementById("menuNav").style.position = "fixed";
+    document.getElementById("menuNav").style.padding = "10px 0px";
+    document.getElementById("menuNav").style.background = "#000";
+
+
+  } else {
+    document.getElementById("menuNav").style.position = "static";
+    document.getElementById("menuNav").style.padding = "40px 0px";
+    document.getElementById("menuNav").style.background = "none";
+  }
+}
+
+
+var map;
+      function initMap() {
+        map = new google.maps.Map(document.getElementById('map'), {
+          center: {lat: 49.987970, lng: 36.240760},
+          zoom: 10
+          
+        });
+      }
