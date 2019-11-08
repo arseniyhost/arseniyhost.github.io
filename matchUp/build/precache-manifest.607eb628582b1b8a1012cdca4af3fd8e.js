b63775ae5adf4d588eb4ba4e2c6b75bad8037b1c
arseniyhost <pilipenko_arseniy@mail.ru>
2019-11-08 14:57:44 +0200

    matchup

---
+++ matchUp/build/precache-manifest.607eb628582b1b8a1012cdca4af3fd8e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "753569b4878ef01849cdff35a27902fb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1dd33302c19d5d4e6660",
+    "url": "/static/css/main.5faf0494.chunk.css"
+  },
+  {
+    "revision": "20179439d3ba38bd6e6e",
+    "url": "/static/js/2.cef0de37.chunk.js"
+  },
+  {
+    "revision": "1dd33302c19d5d4e6660",
+    "url": "/static/js/main.51fdb566.chunk.js"
+  },
+  {
+    "revision": "7074c2a29702854b5809",
+    "url": "/static/js/runtime-main.ae7d8b6e.js"
+  },
+  {
+    "revision": "9039d460f534f78edd607e6c96807870",
+    "url": "/static/media/logoMatch.9039d460.jpg"
+  }
+]);
