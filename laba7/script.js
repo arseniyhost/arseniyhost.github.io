5a5a6b826fc6e29a99c7b14616e75c406f094d4c
Arseniy <arsen.pilipenko2014@gmail.com>
2019-12-23 20:55:38 +0200

    laba7

--- laba7/script.js
+++ laba7/script.js
@@ -1,6 +1,12 @@
 let acc = Math.pow(10, -4);
 
 const F = (x) => {
+	if(x > 0) {
+		let xx = document.createElement('p');
+		xx.innerHTML = `x: ${x}`;
+		const containerX = document.querySelector(".containerX");
+		containerX.append(xx);
+	}
 	return  Math.cos(3 * x) - Math.sin(x) - 0.1;
 }
 
@@ -25,7 +31,7 @@ while(err > acc &&  F(x) != 0) {
 }
 
 const p2 = document.createElement("p");
-p2.innerHTML = `x =  ${x.toFixed(3)}`;
+p2.innerHTML = `x =  ${x.toFixed(4)}`;
 
 const p = document.createElement("p");
 p.innerHTML = `e  = ${acc}`;
@@ -34,7 +40,8 @@ const div = document.createElement("div");
 div.append(p);
 div.append(p2);
 
-document.body.append(div);
+const result = document.querySelector(".result");
+result.append(div);
 
 
 
